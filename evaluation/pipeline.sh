ada8254880b664a735db6cb686db7c56cb112bcf
Elena Adamantidou <39264491+adamelen@users.noreply.github.com>
2020-11-25 12:02:47 +0200

    Update pipeline.sh

--- evaluation/pipeline.sh
+++ evaluation/pipeline.sh
@@ -1,15 +1,3 @@
-# Run AC-SUM-GAN for 10 values of the regularization factor
-bash run_splits.sh 0.1
-bash run_splits.sh 0.2
-bash run_splits.sh 0.3
-bash run_splits.sh 0.4
-bash run_splits.sh 0.5
-bash run_splits.sh 0.6
-bash run_splits.sh 0.7
-bash run_splits.sh 0.8
-bash run_splits.sh 0.9
-bash run_splits.sh 1.0
-
 # Run the evaluation script with the right arguments to compute the F-Scores and extract the loss values for each value of the regularization factor (sigma)
 bash evaluate_exp.sh <path_to_experiment> sigma0.1 TVSum avg
 bash evaluate_exp.sh <path_to_experiment> sigma0.2 TVSum avg
